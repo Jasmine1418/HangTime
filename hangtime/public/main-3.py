38de0b82360d24999df7b7cf744ca3b182874884
Jamarri White <74019796+jamarricodesalot@users.noreply.github.com>
2021-12-14 12:07:11 -0500

    Add files via upload

---
+++ hangtime/public/main-3.py
@@ -0,0 +1,3 @@
+import random
+a = ["firebase","coding","stack","banana","encyclopedia","neutral","extravaganza","potential","flower", "score","necessity","planet","frolicking","trooper", "ability", "fable","enchant", "waterfall", "splendid", "superior", "stagnant","terrific","variety","ethereal","combat","museum","leader","warrior","cowboy","dilated","sphinx","emperor","airplane","breakfast","tithes","sacrificial","loading","global","keyboard","adventure","voyage","trapped","scored","causation","formation","pregnancy","trimester","theoretical","probability","symmetry"]
+print(random.choice(a))
